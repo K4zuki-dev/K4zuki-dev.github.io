70dba637d3a790c45c236654d292cdd4547cfc9d
K4zuki-dev <113984501+K4zuki-dev@users.noreply.github.com>
2023-07-07 01:33:56 +0200

    Fixed File structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-
-import Landing from "@/app/Landing-Div/landing-div";
+import Landing from "@/Landing-Div/landing-div";
 import OurWork from "@/our-work/workComponent";
 import Contact from "@/contact/contact";
 
