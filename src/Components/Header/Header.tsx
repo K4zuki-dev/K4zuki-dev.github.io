c4bb510266a2aeace262e9b960630abc07c2a8dd
K4zuki-dev <113984501+K4zuki-dev@users.noreply.github.com>
2023-07-07 12:59:46 +0200

    Fixed path to image

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -35,7 +35,7 @@ export default function Header() {
 
                 <div className={styles.header_top}>
                     <div className={styles.logo}>
-                        <Image src="/images/Logo.jpg" alt="Our Logo" fill={true} className={styles.logo} />
+                        <Image src="/images/logo.jpg" alt="Our Logo" fill={true} className={styles.logo} />
                     </div>
 
                     <nav className={styles.navBar}>
