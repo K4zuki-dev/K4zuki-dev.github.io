4414588bc305b19077540447efb9e657ed5811d6
K4zuki-dev <113984501+K4zuki-dev@users.noreply.github.com>
2023-07-08 00:40:24 +0200

    Fixed horizontal scrolling, responsive design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 import Footer from '@/Components/Footer/Footer'
-import Header from '@/Components/Header/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,7 +22,7 @@ export default function RootLayout({children,}: {children: React.ReactNode}) {
     <html lang="en">
       <body className={inter.className}>
             {children}
-          <Footer></Footer>
+            <Footer></Footer>
         </body>
     </html>
   )
