1fbc4b74a821a82eb2691192e116ee4eee3b48fb
K4zuki-dev <113984501+K4zuki-dev@users.noreply.github.com>
2023-07-07 12:29:13 +0200

    Commented out API (not working yet)

--- src/app/api/sites/route.ts
+++ src/app/api/sites/route.ts
@@ -1,21 +1,21 @@
-import clientPromise from '@/app/lib/mongodb/mongoClient'
-import { NextResponse } from 'next/server';
+// import clientPromise from '@/app/lib/mongodb/mongoClient'
+// import { NextResponse } from 'next/server';
 
-let client;
-let db;
-let sites;
+// let client;
+// let db;
+// let sites;
 
-export async function GET(){
-    try {
+// export async function GET(){
+//     try {
 
-        client = await clientPromise;
-        db =  client.db("Work");
-        sites =  await db.collection("Work").find({}).toArray();
+//         client = await clientPromise;
+//         db =  client.db("Work");
+//         sites =  await db.collection("Work").find({}).toArray();
         
-    } catch (error) {
-        throw new Error("Failed to establish connection to the database")
-    }
+//     } catch (error) {
+//         throw new Error("Failed to establish connection to the database")
+//     }
 
-    return NextResponse.json( sites )
+//     return NextResponse.json( sites )
     
-}
+// }
