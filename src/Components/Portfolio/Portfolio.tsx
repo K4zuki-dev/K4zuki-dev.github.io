632573fc51cf502234adc38880129ccf673ca1ac
K4zuki-dev <113984501+K4zuki-dev@users.noreply.github.com>
2023-07-10 14:38:57 +0200

    Updated API URL

--- src/Components/Portfolio/Portfolio.tsx
+++ src/Components/Portfolio/Portfolio.tsx
@@ -61,7 +61,7 @@ const imageFade = {
   },
 };
 async function getData() {
-  const res = await fetch("https://k4zuki-dev.github.io/api/sites", {
+  const res = await fetch("https://k4zuki-dev-github-io.vercel.app/api/sites", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
