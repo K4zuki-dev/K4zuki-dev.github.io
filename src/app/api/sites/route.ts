1d7083a7bdaea8cb666fee65db3b53c83aa08e40
K4zuki-dev <113984501+K4zuki-dev@users.noreply.github.com>
2023-07-10 19:42:00 +0200

    Fixed route.ts of api/sites

--- src/app/api/sites/route.ts
+++ src/app/api/sites/route.ts
@@ -1,27 +1,27 @@
-// import { NextResponse } from "next/server";
-// import app from "@/app/lib/firebase/fireClient";
-// import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { NextResponse } from "next/server";
+import app from "@/app/lib/firebase/fireClient";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-// export async function GET() {
-//   try {
-//     var data: any[] = [];
-//     const db = getFirestore(app);
-//     const collectionRef = collection(db, "Sites");
+export async function GET() {
+  try {
+    var data: any[] = [];
+    const db = getFirestore(app);
+    const collectionRef = collection(db, "Sites");
 
-//     await getDocs(collectionRef).then((snapshot) => {
-//       snapshot.docs.map((doc) => {
-//         data.push({ ...doc.data() });
-//       });
-//     });
+    await getDocs(collectionRef).then((snapshot) => {
+      snapshot.docs.map((doc) => {
+        data.push({ ...doc.data() });
+      });
+    });
 
-//     const StringifiedData = JSON.stringify(data);
-//     const Sites: Sites = JSON.parse(StringifiedData);
+    const StringifiedData = JSON.stringify(data);
+    const Sites: Sites = JSON.parse(StringifiedData);
 
-//     return NextResponse.json(Sites);
+    return NextResponse.json(Sites);
 
-//   } catch (error) {
-//     throw new Error("Failed to establish connection to the database");
-//   }
-// }
+  } catch (error) {
+    throw new Error("Failed to establish connection to the database");
+  }
+}
 
 export {}
